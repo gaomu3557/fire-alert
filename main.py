b3ae6ac91ac91c569af648b996e6fbee2e0533e7
gaomu3557 <yakyudaisukimiku0724@gmail.com>
2026-04-09 01:10:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
+import requests
+import os
+
 def main():
-    send_line("🚒【テスト通知】正常に動いています")
+    token = os.environ["LINE_TOKEN"]
+
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+
+    data = {
+        "message": "🚒 テスト通知（これが来れば成功）"
+    }
+
+    r = requests.post(
+        "https://notify-api.line.me/api/notify",
+        headers=headers,
+        data=data
+    )
+
+    print(r.status_code)
+    print(r.text)
+
+if __name__ == "__main__":
+    main()
